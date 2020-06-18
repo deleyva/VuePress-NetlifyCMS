639b20d79b5652fceca19ea2e586c331f3f128c1
deleyva <jesuslopezdeleyva@gmail.com>
2020-06-18 15:34:41 +0200

    actualizo headers y netlifycms version

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,18 +4,34 @@ module.exports = {
   dest: 'pages',
   themeConfig: {
 
-    nav: [
-      { text: 'Home', link: '/' },
+    nav: [{
+        text: 'Home',
+        link: '/'
+      },
       {
         text: 'Projects',
-        items: [
-          { text: 'URLStack', link: 'https://abc.imst.xyz' },
-          { text: 'Hexo-NetlifyCMS', link: 'https://hexocms.imst.xyz' },
-          { text: 'Gitbook-NetlifyCMS', link: 'https://gitbook.imst.xyz' }
+        items: [{
+            text: 'URLStack',
+            link: 'https://abc.imst.xyz'
+          },
+          {
+            text: 'Hexo-NetlifyCMS',
+            link: 'https://hexocms.imst.xyz'
+          },
+          {
+            text: 'Gitbook-NetlifyCMS',
+            link: 'https://gitbook.imst.xyz'
+          }
         ]
       },
-      { text: 'About', link: 'about.md' },
-      { text: 'Admin', link: 'https://vuepress.imst.xyz/admin/' }
+      {
+        text: 'About',
+        link: 'about.md'
+      },
+      {
+        text: 'Admin',
+        link: 'https://reverent-booth-4bcea0.netlify.app/admin/'
+      }
     ],
 
     sidebar: 'auto',
@@ -39,7 +55,7 @@ module.exports = {
 
 
     // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    repo: 'DemoMacro/VuePress-NetlifyCMS',
+    repo: 'deleyva/VuePress-NetlifyCMS',
     // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
     // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
     repoLabel: 'Github',
@@ -55,6 +71,6 @@ module.exports = {
     // 默认是 false, 设置为 true 来启用
     editLinks: true,
     // 默认为 "Edit this page"
-    editLinkText: '帮助我们改善此页面！'
+    editLinkText: 'Edit this page'
   }
 }
